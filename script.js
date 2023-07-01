c7e44a244e973f6d10514c68b8d306c4494f03d1
Victor <victormartinsjob@gmail.com>
2023-07-01 16:31:05 -0300

    Primeira entrega

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ const seaBoard = [
 /*
   Desenvolva seu código logo abaixo
 */
+
+function allocateShips(shipPositions, initialBoard) {
+  for (let i = 0; i < shipPositions.length; i++) {
+    let ship = shipPositions[i]
+    initialBoard[ship[0]][ship[1]] = "S"
+  }
+  return initialBoard
+}
+
+function checkGuesses(guesses, mountedBoard) {
+  for (let i = 0; i < guesses.length; i++) {
+    let guess = guesses[i]
+    if (mountedBoard[guess[0]][guess[1]] == "S") {
+      mountedBoard[guess[0]][guess[1]] = "X"
+    }
+  }
+  return mountedBoard
+}
+
+function checkWinCondition(guessedBoard) {
+  for (let i = 0; i < guessedBoard.length; i++) {
+    for (let j = 0; j < guessedBoard[i].length; j++) {
+      if (guessedBoard[i][j] == "S") {
+        return false
+      }
+    } 
+  } 
+  return true
+}
